bd93b04708691fc1cb3048cf26a2deff95d46b08
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2023-08-15 14:38:18 +0100

    initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function() {
+    let result = "";
 
+    for (let i = 0; i < Math.floor(Math.random() * 100 + 1); i++) {
+       result += "hey";
+    };
+    return result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
